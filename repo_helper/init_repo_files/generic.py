340cf749534476caae9fd36c22a3ad4dd0def739
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-05 23:08:12 +0100

    Add __init__.py files for different licenses.

---
+++ repo_helper/init_repo_files/generic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#
+#  __init__.py
+"""
+{{ short_desc }}
+"""
+#
+#  Copyright © {{ copyright_years }} {{ author }} <{{ email }}>
+#  License: {{ license }}
+#  See the LICENSE file for details.
+#
+
+__author__: str = "{{ author }}"
+__copyright__: str = "{{ copyright_years }} {{ author }}"
+
+__license__: str = "{{ license }}"
+__version__: str = "{{ version }}"
+__email__: str = "{{ email }}"
---
+++ repo_helper/init_repo_files/gpl3_plus.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+#  __init__.py
+"""
+{{ short_desc }}
+"""
+#
+#  Copyright © {{ copyright_years }} {{ author }} <{{ email }}>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+__author__: str = "{{ author }}"
+__copyright__: str = "{{ copyright_years }} {{ author }}"
+
+__license__: str = "{{ license }}"
+__version__: str = "{{ version }}"
+__email__: str = "{{ email }}"
