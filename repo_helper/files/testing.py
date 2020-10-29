6ffc09e9f7e440c428dd2560b36e035e85320024
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-29 08:13:13 +0000

    Use mainline branch of flake8-rst-docstrings-sphinx

--- repo_helper/files/testing.py
+++ repo_helper/files/testing.py
@@ -270,7 +270,7 @@ class ToxConfig(IniConfigurator):
 				"git+https://github.com/PyCQA/pydocstyle@5118faa7173b0e5bbc230c4adf628758e13605bf",
 				"git+https://github.com/domdfcoding/flake8-quotes.git",
 				"git+https://github.com/domdfcoding/flake8-rst-docstrings.git",
-				"git+https://github.com/domdfcoding/flake8-rst-docstrings-sphinx.git@cli",
+				"git+https://github.com/domdfcoding/flake8-rst-docstrings-sphinx.git",
 				"pygments>=2.7.1",
 				])
 		# cmd = f"flake8 {' '.join(self.get_source_files())} --format=rst-toolbox"
