398c3f186d8b869484b14ed05c2ab00c1cd7b4a2
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-09 22:39:42 +0000

    Bump pre-commit-hooks version.

--- repo_helper/files/pre_commit.py
+++ repo_helper/files/pre_commit.py
@@ -224,7 +224,7 @@ def make_pre_commit(repo_path: pathlib.Path, templates: jinja2.Environment) -> L
 
 	domdfcoding_hooks = Repo(
 			repo=make_github_url("domdfcoding", "pre-commit-hooks"),
-			rev="v0.0.4",
+			rev="v0.1.1",
 			hooks=[
 					{"id": "requirements-txt-sorter", "args": ["--allow-git"]},
 					{
