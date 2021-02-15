e615058eec5e85230a8e34b1f460124f3385ffe1
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-02-15 22:12:27 +0000

    Bump formate pre-commit hook version.

--- repo_helper/files/pre_commit.py
+++ repo_helper/files/pre_commit.py
@@ -264,7 +264,7 @@ def make_pre_commit(repo_path: pathlib.Path, templates: jinja2.Environment) -> L
 
 	formate = Repo(
 			repo=make_github_url("repo-helper", "formate"),
-			rev="v0.3.1",
+			rev="v0.3.2",
 			hooks=[{"id": "formate", "exclude": formate_excludes}],
 			)
 
