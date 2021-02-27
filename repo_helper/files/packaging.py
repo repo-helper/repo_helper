11a1e956c74c52d533dfce28f2e3838edb4076b8
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-02-27 10:29:31 +0000

    Don't create unnecessary empty tables in pyproject.toml

--- repo_helper/files/packaging.py
+++ repo_helper/files/packaging.py
@@ -218,6 +218,9 @@ def make_pyproject(repo_path: pathlib.Path, templates: jinja2.Environment) -> Li
 					templates.globals["import_name"].replace('.', '/'),
 					)
 
+	if not data["tool"]:
+		del data["tool"]
+
 	if not templates.globals["enable_tests"]:
 		data["tool"]["importcheck"] = data["tool"].get("importcheck", {})
 
