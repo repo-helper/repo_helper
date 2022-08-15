7d696921c936a1f2c97e471f8758b1885cce969d
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-08-15 11:19:11 +0100

    Bump Python 3.11 pre-release in GitHub Actions

--- repo_helper/utils.py
+++ repo_helper/utils.py
@@ -467,9 +467,9 @@ def set_gh_actions_versions(py_versions: Iterable[str]) -> List[str]:
 	if "3.10-dev" in py_versions:
 		py_versions[py_versions.index("3.10-dev")] = "3.10"
 	if "3.11-dev" in py_versions:
-		py_versions[py_versions.index("3.11-dev")] = "3.11.0-beta.5"
+		py_versions[py_versions.index("3.11-dev")] = "3.11.0-rc.1"
 	if "3.11" in py_versions:
-		py_versions[py_versions.index("3.11")] = "3.11.0-beta.5"
+		py_versions[py_versions.index("3.11")] = "3.11.0-rc.1"
 	if "pypy3" in py_versions:
 		py_versions[py_versions.index("pypy3")] = "pypy-3.6"
 	if "pypy36" in py_versions:
