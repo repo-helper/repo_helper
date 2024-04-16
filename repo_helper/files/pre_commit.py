b7a0280c58ba0779dbec3ac46b5ed61c65990102
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-04-16 11:17:35 +0100

    Bump pyproject-parser pre-commit hook

--- repo_helper/files/pre_commit.py
+++ repo_helper/files/pre_commit.py
@@ -205,7 +205,7 @@ flake2lint = Repo(
 
 pyproject_parser = Repo(
 		repo=make_github_url("repo-helper", "pyproject-parser"),
-		rev="v0.9.1",
+		rev="v0.11.0",
 		hooks=["reformat-pyproject"],  # TODO: add check-pyproject
 		)
 
