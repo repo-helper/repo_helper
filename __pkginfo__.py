b1f4c9969d1d020314c8f9c6c272c7c3ab122baa
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-08-12 12:32:42 +0100

    Put pinned dulwich requirement in a `cli` extra.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -4,5 +4,6 @@ __all__ = ["extras_require"]
 
 extras_require = {
 		"testing": ["check-wheel-contents>=0.2.0", "coincidence>=0.2.0", "pytest>=6.2.0"],
-		"all": ["check-wheel-contents>=0.2.0", "coincidence>=0.2.0", "pytest>=6.2.0"]
+		"cli": ["dulwich<=0.20.33,>=0.20.5"],
+		"all": ["check-wheel-contents>=0.2.0", "coincidence>=0.2.0", "dulwich<=0.20.33,>=0.20.5", "pytest>=6.2.0"]
 		}
