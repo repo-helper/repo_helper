fbc4acde2c5c3f233fad9b80026aaf12a2896a8e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-07-13 15:52:48 +0100

    Bump domdfcoding/pre-commit-hooks

--- repo_helper/files/pre_commit.py
+++ repo_helper/files/pre_commit.py
@@ -231,7 +231,7 @@ snippet_fmt = Repo(
 
 domdfcoding_hooks = Repo(
 		repo=make_github_url("domdfcoding", "pre-commit-hooks"),
-		rev="v0.7.0",
+		rev="v0.8.0",
 		hooks=[
 				{"id": "requirements-txt-sorter", "args": ["--allow-git"]},
 				{"id": "check-docstring-first", "exclude": r"^(doc-source/conf|__pkginfo__|setup|tests/.*)\.py$"},
